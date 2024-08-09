48ef88dff38dc7a068bb1adf871c142216d14ed1
JuliusB-ui <141216908+JuliusB-ui@users.noreply.github.com>
2024-08-09 23:14:08 +0800

    adding html draft + svg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,120 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+      <logo>ExpenseTracker</logo>
+      <ul Transaction History>
+        <li><button>All Expense</button></li>
+        <li><button>All Income</button></li>
+      </ul>      
+      <div dropdown>
+        <button>
+          <img></img>
+          name
+          <svg></svg>
+        </button>
+        <div dropdown-menu>
+          <a dropdown-item><svg></svg>Profile Settings</a>
+          <a dropdown-item><svg></svg>Log out</a>
+        </div>
+      </div>
+    </header>
+    <main non-auth>
+      <div>
+        <img bgimgwrapper/>
+        <img decorativetab/>
+      </div>
+
+      <div Welcome Page>
+        <div>
+          <h3>EXPENSE LOG</h3>
+          <h1>Manage Your <span>Finances</span> Masterfully!</h1>
+          <p>ExpenseTracker effortlessly empowers you to take control of your finances! With intuitive features, it simplifies the process of tracking and managing expenses, allowing for a stress-free mastery over your financial world.</p>
+          <ul Auth link>
+            <button>Sign Up</button>
+            <button>Sign In</button>
+          </ul>
+        </div>
+        <div allUsers>
+          <div images>
+            <div><img/></div>
+            <div><img/></div>
+            <div><img/></div>
+          </div>
+          <div>
+            <h2>1000 users +</h2>
+            <p>Trusted by users for reliable expense tracking!</p>
+          </div>
+        </div>
+      </div>
+
+      <div Register Page>
+        <h1>Sign Up</h1>
+        <p>Step into a world of hassle-free expense management! Your journey towards financial mastery begins here.</p>
+        <form>
+          <label htmlFor="name"><input id="name" type="name"/></label>
+          <label htmlFor="email"> <input id="email" type="email"/></label>
+          <label htmlFor="password"> <input id="password" type="password / text"/> <span><svg></svg></span></label>
+          <button>Sign Up</button>
+        </form>
+        <p>Already have account? <a>Sign In</a></p>
+      </div>
+      <div Login Page>
+        <h1>Sign In</h1>
+        <p>Welcome back to effortless expense tracking! Your financial dashboard awaits.</p>
+        <form>
+            <label htmlFor="email"><input id="email" type="email" name="email"/></label>
+            <label htmlFor="password"> <input id="password" type="password / text"/> <span><svg></svg></span></label>
+            <button>Sign In</button>
+        </form>
+        <p>Don't have an account? <a>Sign Up</a></p>
+      </div>
+    </main>
+
+    <main>
+      <div Main Page>
+        <div>
+          <h1>Expense Log</h1>
+          <p>Capture and organize every penny spent with ease! A clear view of your financial habits at your fingertips.</p>
+        </div>
+        <div income total>
+          <svg></svg>
+          <div>
+            <h4>Total Income</h4>
+            <h3>amount</h3>
+          </div>
+        </div>
+        <div expense total>
+          <svg></svg>
+          <div>
+            <h4>Total Expense</h4>
+            <h3>amount</h3>
+          </div>
+        </div>
+        <div expenses category>
+          <h4>Expenses categories</h4>
+          <div>
+          <piechart></piechart>
+          <ul>
+            expenses
+          </ul>
+          </div>
+        </div>
+      </div>
+      <form>
+        <div>
+          <input type="radio" name="expense" id="exepenseradio1" />
+          <label htmlFor="expenseradio1">Expense</label>
+          <input type="radio" name="income" id="incomeradio1" />
+          <label htmlFor="incomeradio1">Income</label>
+        </div>
+        <div>
+          <label htmlFor="date">Date<input type="date" name="date" id="date1"/><span><a><svg></svg></a></span></label>
+          <label htmlFor="time">Time<input type="time" name="time" id="time1" /><span><a><svg></svg></a></span></label>
+        </div>
+        <label htmlFor="category">Category <select name="category" id="category1"></select></label>
+      </form>
+    </main>
     </div>
   );
 };
